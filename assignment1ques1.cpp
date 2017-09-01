d48dedfc299ae6a60ac9d93abb121723ba0fda19
LoknathBehera2112 <31297544+LoknathBehera2112@users.noreply.github.com>
2017-09-01 21:48:33 +0530

    Add files via upload

---
+++ assignment1ques1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int i;
+char c;
+bool b;
+double d;
+float f;
+cout<<"Enter the required integer,character,bool,double,float in sequence";
+cin>>i>>c>>b>>d>>f;
+cout<<"\ninteger= "<<i;
+cout<<"\ncharacter= "<<c;
+cout<<"\nbool= "<<b;
+cout<<"\ndouble= "<<d;
+cout<<"\ndecimal= "<<f;// your code goes here
+	return 0;
+}
